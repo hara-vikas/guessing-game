b30eb4e70f56f0c38e14a783759327c0c94e2261
hara-vikas <55883694+hara-vikas@users.noreply.github.com>
2019-09-28 14:12:04 +0530

    Update

--- Guessing_Game.py
+++ Guessing_Game.py
@@ -9,30 +9,30 @@ print('if you are very near to the number it shows - warmer')
 print('if you are farther than the number before it shows -colder', '\nlets start')
 ALGUESS, PREGUESS, FLAG = 0, 0, 0
 while True:                                                        #while loop for iteratons
-	if FLAG == 1:
-		PREGUESS = GUESS
-		
-	GUESS = int(input('enter your guess'))
-	ALGUESS = ALGUESS+1
-	if GUESS in range(1, 101):
-		if GUESS == GEN:
-			print('congrats your guess is correct')         #if statements for checking conditions
-			print("number of guesses", ALGUESS)
-			exit()
-		elif abs(GEN-GUESS) <= 10 and GEN > GUESS and FLAG == 0:
-			FLAG = 1
-			print('warm')
-			continue
-		elif abs(GEN-GUESS) <= 10 and GEN < GUESS and FLAG == 0:
-			FLAG = 1
-			print('cold')
-			continue
-		elif abs(GEN-GUESS) < abs(GEN-PREGUESS) and FLAG == 1:
-			print('warmer')
-			continue
-		elif abs(GEN-GUESS) > abs(GEN-PREGUESS) and FLAG == 1:
-			print('colder')
-			continue
-	else:
-		print('out of bounds')
-		print('number should lie between 1 to 100')
+   if FLAG == 1:
+       PREGUESS = GUESS
+	
+   GUESS = int(input('enter your guess'))
+   ALGUESS = ALGUESS+1
+   if GUESS in range(1, 101):
+       if GUESS == GEN:
+	   print('congrats your guess is correct')         #if statements for checking conditions
+	   print("number of guesses", ALGUESS)
+	   exit()
+       elif abs(GEN-GUESS) <= 10 and GEN > GUESS and FLAG == 0:
+	   FLAG = 1
+	   print('warm')
+	   continue
+       elif abs(GEN-GUESS) <= 10 and GEN < GUESS and FLAG == 0:
+	   FLAG = 1
+	   print('cold')
+	   continue
+       elif abs(GEN-GUESS) < abs(GEN-PREGUESS) and FLAG == 1:
+	   print('warmer')
+	   continue
+       elif abs(GEN-GUESS) > abs(GEN-PREGUESS) and FLAG == 1:
+	   print('colder')
+	   continue
+   else:
+       print('out of bounds')
+       print('number should lie between 1 to 100')
